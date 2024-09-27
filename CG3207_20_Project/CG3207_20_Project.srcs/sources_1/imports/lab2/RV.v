a80a5d6a27c1c2972f7133b2d148f98b4f3589c2
Marvin Pranajaya <marvinpofficial@gmail.com>
2024-09-27 13:37:59 +0800

    Decoder functionality except linking for jal

--- CG3207_20_Project/CG3207_20_Project.srcs/sources_1/imports/lab2/RV.v
+++ CG3207_20_Project/CG3207_20_Project.srcs/sources_1/imports/lab2/RV.v
@@ -75,7 +75,7 @@ module RV(
     wire RegWrite ;
     //wire MemWrite ;
     wire MemtoReg ;
-    //wire [1:0] ALUSrcA ;
+    wire [1:0] ALUSrcA ;
     wire ALUSrcB ;
     //wire [2:0] ImmSrc ;
     wire [3:0] ALUControl ;
@@ -138,6 +138,7 @@ module RV(
                     RegWrite,
                     MemWrite,
                     MemtoReg,
+                    ALUSrcA,
                     ALUSrcB,
                     ImmSrc,
                     ALUControl
