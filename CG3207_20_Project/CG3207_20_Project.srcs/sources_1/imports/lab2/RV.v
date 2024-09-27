cdf5f6ba3610f1294865d4ae26581fb310422627
Marvin Pranajaya <marvinpofficial@gmail.com>
2024-09-27 13:45:42 +0800

    Added link capability for decoder

--- CG3207_20_Project/CG3207_20_Project.srcs/sources_1/imports/lab2/RV.v
+++ CG3207_20_Project/CG3207_20_Project.srcs/sources_1/imports/lab2/RV.v
@@ -76,7 +76,7 @@ module RV(
     //wire MemWrite ;
     wire MemtoReg ;
     wire [1:0] ALUSrcA ;
-    wire ALUSrcB ;
+    wire [1:0] ALUSrcB ;
     //wire [2:0] ImmSrc ;
     wire [3:0] ALUControl ;
     
