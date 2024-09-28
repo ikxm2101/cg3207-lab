f80ff9e348c5b59746a738db4b8bc355b4442cb8
teohxuen <projectxuen@gmail.com>
2024-09-28 18:13:10 +0800

    added initial lab2 assembly

---
+++ project/software/lab2.asm
@@ -0,0 +1,47 @@
+# This program is to evaluate the Hardware Implementation of RISC-V Processor
+
+# ------- <code memory (ROM mapped to Instruction Memory) begins>
+.text	## IROM segment 0x00000000-0x000001FC
+# Total number of instructions should not exceed 128 (127 excluding the last line 'halt B halt').
+
+main:
+
+
+
+# ------- <code memory (ROM mapped to Instruction Memory) ends>			
+				
+								
+#------- <constant memory (ROM mapped to Data Memory) begins>									
+.data	## DROM segment 0x00002000-0x000021FC
+# All constants should be declared in this section. This section is read only (Only lw, no sw).
+# Total number of constants should not exceed 128
+# If a variable is accessed multiple times, it is better to store the address in a register and use it rather than load it repeatedly.
+DROM:
+DELAY_VAL: .word 4
+
+#------- <constant memory (ROM mapped to Data Memory) ends>	
+
+
+# ------- <variable memory (RAM mapped to Data Memory) begins>
+.align 9 ## DRAM segment. 0x00002200-0x000023FC #assuming rodata size of <= 512 bytes (128 words)
+# All variables should be declared in this section, adjusting the space directive as necessary. This section is read-write.
+# Total number of variables should not exceed 128. 
+# No initialization possible in this region. In other words, you should write to a location before you can read from it (i.e., write to a location using sw before reading using lw).
+DRAM:
+.space 512
+
+# ------- <variable memory (RAM mapped to Data Memory) ends>
+
+
+# ------- <memory-mapped input-output (peripherals) begins>
+.align 9 ## MMIO segment. 0x00002400-0x00002418
+MMIO:
+LEDS: .word 0x0			# 0x00002400	# Address of LEDs. //volatile unsigned int * LEDS = (unsigned int*)0x00000C00#  
+DIPS: .word 0x0			# 0x00002404	# Address of DIP switches. //volatile unsigned int * DIPS = (unsigned int*)0x00000C04#
+PBS: .word 0x0			# 0x00002408	# Address of Push Buttons. Used only in Lab 2
+CONSOLE: .word 0x0		# 0x0000240C	# Address of UART. Used only in Lab 2 and later
+CONSOLE_IN_valid: .word 0x0	# 0x00002410	# Address of UART. Used only in Lab 2 and later
+CONSOLE_OUT_ready: .word 0x0	# 0x00002414	# Address of UART. Used only in Lab 2 and later
+SEVENSEG: .word	0x0		# 0x00002418	# Address of 7-Segment LEDs. Used only in Lab 2 and later
+
+# ------- <memory-mapped input-output (peripherals) ends>
