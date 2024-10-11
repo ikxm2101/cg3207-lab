1e55915c2aa411e65556b7f9c0229713504c1440
Irwin <irwin.kxm@outlook.com>
2024-10-11 21:42:08 +0800

    add multi-cycle instructions decoding in Decoder.sv

--- project/hardware/hardware.srcs/sources_1/new/Decoder.sv
+++ project/hardware/hardware.srcs/sources_1/new/Decoder.sv
@@ -35,18 +35,21 @@ module Decoder(
     input [6:0] Opcode,
     input [2:0] Funct3,
     input [6:0] Funct7,
-    output logic [1:0] PCS,		    // 00 for non-control, 01 for conditional branch, 10 for jal, 11 for jalr
-    output logic RegWrite,		    // Asserted only by instructions which write to register file (load, auipc, lui, DPImm, DPReg);
-    output logic MemWrite,		    // Asserted only by store (sw)
-    output logic MemtoReg,		    // Asserted only by load (lw)
-    output logic [1:0] ALUSrcA,     // Needed for lui, auipic. Refer to the microarchitecture for its use. Uncomment wire and port map in RV.v as well
-    output logic ALUSrcB,		    // Asserted by all instructions which use an immediate (load, store, lui, auipc, DPImm). Needs to be expanded to a 2-bit signal to support link functionality for jal, jalr. Change wire width in RV.v as well
-    output logic [2:0] ImmSrc, 	    // 000 for U, 010 for UJ, 011 for I, 110 for S, 111 for SB.
-    output logic [3:0] ALUControl	// 0000 for add, 0001 for sub, 1110 for and, 1100 for or, 0010 for sll, 1010 for srl, 1011 for sra, 0001 for branch, 0000 for all others.
-                                    // Note that the most significant 3 bits are Funct3 for all DP instrns. LSB is the same as Funct[5] for DPReg type and DPImm_shifts.
-                                    // For other DPImms, Funct[5] is 0.
-                                    // It is the same as sub for branches, and add for all others not mentioned in the line above.
-    ); 
+    output logic [1:0] PCS,		        // 00 for non-control, 01 for conditional branch, 10 for jal, 11 for jalr
+    output logic RegWrite,		        // Asserted only by instructions which write to register file (load, auipc, lui, DPImm, DPReg);
+    output logic MemWrite,		        // Asserted only by store (sw)
+    output logic MemtoReg,		        // Asserted only by load (lw)
+    output logic [1:0] ALUSrcA,         // Needed for lui, auipic. Refer to the microarchitecture for its use. Uncomment wire and port map in RV.v as well
+    output logic ALUSrcB,		        // Asserted by all instructions which use an immediate (load, store, lui, auipc, DPImm). Needs to be expanded to a 2-bit signal to support link functionality for jal, jalr. Change wire width in RV.v as well
+    output logic [2:0] ImmSrc, 	        // 000 for U, 010 for UJ, 011 for I, 110 for S, 111 for SB.
+    output logic [3:0] ALUControl,	    // 0000 for add, 0001 for sub, 1110 for and, 1100 for or, 0010 for sll, 1010 for srl, 1011 for sra, 0001 for branch, 0000 for all others.
+                                        // Note that the most significant 3 bits are Funct3 for all DP instrns. LSB is the same as Funct[5] for DPReg type and DPImm_shifts.
+                                        // For other DPImms, Funct[5] is 0.
+                                        // It is the same as sub for branches, and add for all others not mentioned in the line above.
+    output logic MCycleStart,           // Asserted only by instructions which start a multi-cycle operation (eg. mul, divu)
+    output logic MCycle_ResultSelect,   // Selection signal to choose between MCycle_Result1 and MCycle_Result2 depending on the operation
+    output logic [1:0] MCycleOp         // Multi-cycle Operation. "00" for signed multiplication, "01" for unsigned multiplication, "10" for signed division, "11" for unsigned division.
+    );
     // Change wire to reg if assigned inside a procedural (always) block. However, where it is easy enough, use assign instead of always.
     // A 2-1 multiplexing can be done easily using an assign with a ternary operator
     // For multiplexing with number of inputs > 2, a case construct within an always block is a natural fit. DO NOT to use nested ternary assignment operator as it hampers the readability of your code.
@@ -77,8 +80,32 @@ module Decoder(
     localparam AUIPC = 7'h17;
     localparam LUI = 7'h37;
     
-    assign MemtoReg = (Opcode == LOAD) ? 1'b1 : 1'b0; // Only for load
-    assign MemWrite = (Opcode == STORE) ? 1'b1 : 1'b0; // Only for store
+    /* Funct3 for mul and divu
+        * mul       0x00 (000)
+        * mulh      0x01 (001)
+        * mulsu     0x02 (010)
+        * mulu      0x03 (011)
+        * div       0x04 (100)
+        * divu      0x05 (101)
+        * rem       0x06 (110)
+        * remu      0x07 (111)
+    */
+    localparam FUNCT3_MUL = 3'h0;
+    localparam FUNCT3_MULH = 3'h1;
+    localparam FUNCT3_MULSU = 3'h2;
+    localparam FUNCT3_MULU = 3'h3;
+    localparam FUNCT3_DIV = 3'h4;
+    localparam FUNCT3_DIVU = 3'h5;
+    localparam FUNCT3_REM = 3'h6;
+    localparam FUNCT3_REMU = 3'h7;
+
+    /* Funct7 for DP Reg
+        * Most base instructions    0x00 (0000000)
+        * sub, sra                  0x20 (0100000)
+        * M extension               0x01 (0000001)
+    */
+
+    localparam FUNCT7_M = 7'h01;
 
     assign RegWrite = (Opcode == STORE || Opcode == BRANCH || Opcode == JAL) ? 1'b0 : 1'b1; // Only for DP Reg, DP Imm, load, auipc, lui
 
@@ -119,7 +146,59 @@ module Decoder(
             default: ALUControl = 4'b0000;
         endcase
     end
+    
+    always_comb begin : MCycleStartBlock
+        case (Funct7)
+            FUNCT7_M: MCycleStart = 1'b1;
+            default: MCycleStart = 1'b0;
+        endcase
+    end
 
+    always_comb begin : MCycle_ResultSelectBlock
+       /*
+        * 0 for MCycle_Result1
+        * 1 for MCycle_Result2
+        */
+        case ({ Funct3, Funct7 })
+            { FUNCT3_MUL, FUNCT7_M }: MCycle_ResultSelect = 1'b0;
+            { FUNCT3_MULH, FUNCT7_M }: MCycle_ResultSelect = 1'b1;
+            { FUNCT3_MULSU, FUNCT7_M }: MCycle_ResultSelect = 1'bx; // not implemented
+            { FUNCT3_MULU, FUNCT7_M }: MCycle_ResultSelect = 1'b1;
+            { FUNCT3_DIV, FUNCT7_M }: MCycle_ResultSelect = 1'b0;
+            { FUNCT3_DIVU, FUNCT7_M }: MCycle_ResultSelect = 1'b0;
+            { FUNCT3_REM, FUNCT7_M }: MCycle_ResultSelect = 1'b1;
+            { FUNCT3_REMU, FUNCT7_M }: MCycle_ResultSelect = 1'b1;
+            default: MCycle_ResultSelect = 1'bx;
+        endcase
+    end
+
+    always_comb begin : MCycleOpBlock
+        case ({ Funct3, Funct7 })
+        /*
+         * Instruction  | MCycleOp[1:0]
+         * multiply (S) | 00  
+         * multiply (U) | 01
+         * division (S) | 10
+         * division (U) | 11
+        */
+        /*
+          ! for mul:
+          ! use the unsigned multiplication operation because it takes 
+          ! less cycles to achieve the same result as signed multiplication
+          ! in the current multiplier implementation in MCycle.v 
+         */ 
+            { FUNCT3_MUL, FUNCT7_M }: MCycleOp = 2'b01;
+            { FUNCT3_MULH, FUNCT7_M }: MCycleOp = 2'b00;
+            { FUNCT3_MULSU, FUNCT7_M }: MCycleOp = 2'bxx; // not implemented
+            { FUNCT3_MULU, FUNCT7_M }: MCycleOp = 2'b01;
+            { FUNCT3_DIV, FUNCT7_M }: MCycleOp = 2'b10;
+            { FUNCT3_DIVU, FUNCT7_M }: MCycleOp = 2'b11;
+            { FUNCT3_REM, FUNCT7_M }: MCycleOp = 2'b00;
+            { FUNCT3_REMU, FUNCT7_M }: MCycleOp = 2'b11;
+            default: MCycleOp = 2'bxx;
+        endcase
+    end
+    
 endmodule
 
 
